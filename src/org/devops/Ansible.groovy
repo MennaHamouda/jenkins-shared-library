828330175cd3d727c90a5f197a9c0a3ddf5ad844
Menna Hamouda <mennahamouda2001.com>
2026-08-04 12:40:50 +0300

    fix ansible

--- src/org/devops/Ansible.groovy
+++ src/org/devops/Ansible.groovy
@@ -1,5 +1,7 @@
 package org.devops
 
+import groovy.text.SimpleTemplateEngine
+
 class Ansible implements Serializable {
 
     def steps
@@ -9,14 +11,30 @@ class Ansible implements Serializable {
     }
 
     def configure(String sshKey) {
+        def tf = steps.readJSON(file: 'terraform-output.json')
+        def template = steps.readFile(file: 'ansible/inventory/inventory.ini.tpl')
+
+        def binding = [
+            bastion_public_ip: tf.bastion_public_ip.value,
+            master_private_ip: tf.master_private_ip.value,
+            worker_private_ips: tf.worker_private_ips.value.join('\n'),
+            ansible_user: 'ubuntu',
+            bastion_ssh_proxy: "ubuntu@${tf.bastion_public_ip.value}"
+        ]
+
+        def renderedInventory = new SimpleTemplateEngine()
+            .createTemplate(template)
+            .make(binding)
+            .toString()
+
+        steps.writeFile(file: 'ansible/inventory/inventory.ini', text: renderedInventory)
 
         steps.sh """
         ansible-playbook \
-          -i inventory.ini \
+          -i ansible/inventory/inventory.ini \
           --private-key ${sshKey} \
           ansible/playbook.yml
         """
-
     }
 
 }
